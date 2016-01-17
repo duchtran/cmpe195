def59eec6b2ea226e1c6dd8b007c67cd828e73f5
duchtran <duchongtran@gmail.com>
2016-01-17 15:46:38 -0800

    update app.py and new tests

--- python_gui/image_button.py
+++ python_gui/image_button.py
@@ -2,6 +2,6 @@ from Tkinter import *
 root=Tk()
 b=Button(root,justify = LEFT)
 photo=PhotoImage(file="/home/duc/Dropbox/cmpe195_repository/python_gui/images/play_button.gif")
-b.config(image=photo,width="200",height="200")
+b.config(image=photo,width="100",height="100")
 b.pack(side=LEFT)
 root.mainloop()
